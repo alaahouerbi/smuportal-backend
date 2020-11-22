9968cb30fa5835e00f00ee801a41e3c47f5ec7b2
alaahouerbi <72623505+alaahouerbi@users.noreply.github.com>
2020-11-22 17:02:43 +0100

    attempting to create query for job Applications

--- routes/api/jobApplication.js
+++ routes/api/jobApplication.js
@@ -1,6 +1,8 @@
 const Router = require("express").Router;
 const jobApplicationService = require("../../services/jobApplication.service")();
+const {verifyToken} = require("../../helpers/verifyToken");
 const jobApplication = require("../../models/jobApplication");
+const User = require("../../models/User");
 
 const router = Router({
     mergeParams: true,
@@ -16,5 +18,14 @@ router.get("/getJobApplication", async(req,res)=> {
 });
 
 //route to add A job Application
+router.post("/applyForJob",verifyToken, async(req,res)=>{
+    try{
+        let user=await User.findById(req.decodedToken._id);
+        await jobApplicationService.addJobApplication(req.body,user);
+        res.send({ success: true, msg: "Job application Added" });
+    }catch (err) {
+        res.send({ success: false, msg: "Job application not Added!", err });
+      }
+});
 module.exports = router;
 //router.post
