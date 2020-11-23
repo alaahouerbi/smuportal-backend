228d1458dde6cebe617db355e61dcfb09e1363f0
alaahouerbi <72623505+alaahouerbi@users.noreply.github.com>
2020-11-23 22:53:02 +0100

    working on populate for jobApplication.service

--- services/jobApplication.service.js
+++ services/jobApplication.service.js
@@ -1,9 +1,25 @@
 const jobApplication =require("../models/jobApplication");
+const JobOffer = require("../models/JobOffer");
+const jobOfferService = require("./jobOffer.service");
 
 function jobApplicationService(){
     //function to get JobApplication
     async function getJobApplication(){
         return jobApplication.find({})
+    }
+    //returns job applications made to the job offer the current user created
+    async function getJobApplicationByOfferUser(id){
+        const query ={};
+        return (jobApplication.find(query)
+        .populate({
+            path:'jobOffer',
+            select:"User",
+            populate:{
+                path:"User",
+                select:"_id"
+            }
+        })
+        .lean().exec());
     }
    //function to addAjobApplication needs a job app and a user
    async function addJobApplication(JobOffer,User){
@@ -14,7 +30,8 @@ function jobApplicationService(){
 
 return {
     getJobApplication,
-    addJobApplication
+    addJobApplication,
+    getJobApplicationByOfferUser
 }
 }
 module.exports=jobApplicationService;
